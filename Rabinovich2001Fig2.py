c1cfdb36bca9ab26de4c953f649d3f683f1a0522
Lawson Fuller <llfuller@users.noreply.github.com>
2019-11-07 10:10:34 -0800

    Working Rabinovich 2001 Figure 2 (right graph)
    After replacing the regularly run operation with synapses and (most importantly) shrinking the timestep size to get rid of artifacts and infinities, the recreation of figure 2 in Rabinovich 2001 is done!

--- Rabinovich2001Fig2.py
+++ Rabinovich2001Fig2.py
@@ -1,21 +1,28 @@
 # Attempt at recreating Rabinovich 2001 paper figure 2.
 from brian2 import *
 import scipy as sp
+# print(defaultclock.dt)
+defaultclock.dt = 0.05*ms # Half the usual timestep
 n = 9
 duration = 200*ms # at and after 141ms, this breaks for neurons 4 and 6. Those neurons explode afterward
+
+def Modulation(t):
+    return (t<100*ms) # Reverse Heaviside Step
+
+# Time-varying input current section
 num_samples = int(duration/defaultclock.dt) # 10,000 samples
 # Time-variable external current:
 t_recorded = arange(num_samples)*defaultclock.dt
-A=0.1
-B=0
-f=10/ms
+A=0.5
+f=2*pi/20.0/ms
 # S is 'stimulus'
 # Linear Current
 # S_recorded = TimedArray(A*t_recorded/(ms)/1000, dt=defaultclock.dt) # ms term cancels units of t_recorded
 # Current Steps
 # S_recorded = TimedArray(A*t_recorded/(ms)/1000//0.1*0.1, dt=defaultclock.dt)
 # Sinusoidal Current
-S_recorded = TimedArray(A*cos(B*t_recorded/ms), dt=defaultclock.dt)
+S_recorded = TimedArray(A*cos(f*t_recorded)*Modulation(t_recorded), dt=defaultclock.dt)
+# End of time-varying input current section
 GivenS = [0.1,.15,0,0,.15,0.1,0,0,0]
 
 print(len(t_recorded))
@@ -24,7 +31,7 @@ print(S_recorded.values)
 # Inhibitory max amplitude
 g = zeros((n,n)) #needs to be +1,+1 larger than usual (extra row and column) to make the following clean:
 # Inhibitory pairs: (presynaptic, postsynaptic):
-inhibPairs = [[1,5],[5,2],[2,1],[2,4],[4,5],[5,2],
+inhibPairs = [[1,5],[5,2],[2,1],[2,4],[4,5],
               [6,5],[2,6],[3,6],[5,3],[7,4],[5,7],
               [8,4],[5,8],[8,6],[8,9],[9,5]]
 for pair in inhibPairs:
@@ -35,13 +42,13 @@ eqs = '''
 dv/dt = (v - v**3 /(3) - w - z*(v-nu) + 0.35 + S)/tau1: 1
 dw/dt = (v - b*w + a)/ms : 1 
 dz/dt = (gSumG - z)/tau2 : 1
-tau1 = 0.08 *ms : second # Could be wrong. I don't know if it should be in ms
+tau1 = 0.08 *ms : second 
 tau2 = 3.1 *ms  : second
 a = 0.7 : 1
 b = 0.8 : 1
 nu = -1.5 : 1
-S : 1
-# S = S_recorded(t) : 1
+# S : 1
+S = S_recorded(t) : 1
 G = (v>0) : 1
 gSumG : 1
 '''
@@ -51,29 +58,20 @@ group = NeuronGroup(n, eqs, threshold = 'v > 1.2', method = "rk4")
 group.v = -1.2
 group.w = -0.62
 group.z = 0
-group.S = GivenS
+# group.S = GivenS
 
-@network_operation(dt=defaultclock.dt) # worry: does this lag a frame behind? Also (separately) see documentation pg124
-def inhibition():
-    # for i in range(1,n):
-    #     sum = 0.0
-    #     for j in range(1,n):
-    #         if i!=j:
-    #             multiple = g[j,i]*group.G[j]
-    #             sum += multiple
-    #     group.gSum[i] = sum
-    # The above for loop describes what the following assignment does
-    group.gSumG.set_item(slice(None), sp.dot(sp.transpose(g), sp.array(group.G.__array__())))
-    print(group.gSumG)
-    # pass
+syn = Synapses(group, group, model = 'gSumG_post = 2*(v_pre>0) : 1 (summed)')
+syn.connect(i=[0,3,5,8], j=4)
+syn.connect(i=1, j=[0,5])
+syn.connect(i=2, j=5)
+syn.connect(i=4, j=[1,2,6,7])
+syn.connect(i=6, j=3)
+syn.connect(i=7, j=[3,5,8])
 
 
 monitor = SpikeMonitor(group)
 M = StateMonitor(group, variables=True, record=True)
 run(duration)
-
-# Plot current
-# plot(M.t/ms, M.G[0]*(n/10)/amp, 'y')
 
 # Plot raster of neurons spikes
 for j in range(0,n):
@@ -82,20 +80,15 @@ xlabel('spike time (ms)')
 ylabel('neuron index')
 show()
 
-# print("Length of M: "+str(len(M)))
-# print(M)
-
 # Plot v(t) of neurons
-fig, axs = plt.subplots(n)
+fig, axs = plt.subplots(n+1)
 fig.suptitle('V(t) [maybe] over time')
+axs[0].set(xlabel = 'time (ms)')
 for i in range(n-1,-1,-1):
     axs[i].plot(M.t / ms, M[i].v)
     axs[i].set(ylabel = "V"+str(i+1))
-axs[0].set(xlabel = 'time (ms)')
-# # plot(M.t/ms, M[0].v / mV, color="k")
-# # plot(M.t/ms, M[1].v / mV)
-# # plot(M.t/ms, M[2].v / mV, color="r")
-# plot(t_recorded, S_recorded)
+axs[n].plot(M.t/ms, [S_recorded(t) for t in t_recorded], 'c') # universal driving current
+# axs[n].plot(M.t/ms, M.G[0]*(n/10)/amp, 'y') # current in neuron 0
 show()
 
-# Result: neurons 4 and 6 always blow up right at the end
+# Result: neurons 6 blows up sometime after 100ms and before 200ms
